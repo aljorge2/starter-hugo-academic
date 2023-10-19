8b9257acd447aaac423e8bfb28eea06a9cdad1a7
Annika Jorgensen <annikajorgensen@Annikas-Air.lan>
2023-10-18 21:19:17 -0700

    confusing testing

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# OVERVIEW: This script will be ran every time I update my website
+
+#COMMAND STRUCTURE: bash update.sh YourCommitMessage
+
+git add .
+git commit -m $1
+git push -u origin main
+
+hugo # Generate the website inside the public directory
+cd public
+git add .
+git commit -m $1
+git push origin main
+cd ..
